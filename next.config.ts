c76d0491a2700aab9d05f233cf3c60840d4cbd92
maxenceleguery <maxence.leguery@gmail.com>
2025-08-09 19:07:43 +0200

    :wrench: Fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/portfolio/' : '',
-  basePath: isProd ? '/portfolio' : '',
   output: 'export'
 };
 
